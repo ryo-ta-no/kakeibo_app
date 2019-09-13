2673e38cd47535eee525bd6ddb09a12aa47b1186
ryo-ta-no <kabhi4w@gmail.com>
2019-09-13 13:17:19 +0900

    create variablecosts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_080514) do
+ActiveRecord::Schema.define(version: 2019_09_13_035842) do
 
   create_table "fixedcosts", force: :cascade do |t|
     t.string "name", null: false
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_09_12_080514) do
   end
 
   create_table "incomes", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "variablecosts", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
     t.datetime "created_at", null: false
